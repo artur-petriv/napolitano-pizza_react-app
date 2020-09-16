d89433b42ea31937e009bc1bf6226c883e606841
Artur Petriv <artur.petriv@gmail.com>
2020-09-16 19:16:01 +0300

    Complite header components markup

--- napolitano/src/components/header/Header.jsx
+++ napolitano/src/components/header/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import logoSvg from "./logo.svg";
+import deliverySvg from "./delivery.svg";
+import clockSvg from "./clock.svg";
+import callSvg from "./call.svg";
 
 // Styles
-import "../main.sass";
+import "../Main.sass";
 import "./Header.sass";
 
 function Header() {
   return (
     <div className="header">
-      <div className="container">
+      <div className="header-tip">
+        <div className="header-tip__title">Час доставки</div>
+        <div className="header-tip__text">~40 хвилин</div>
+      </div>
+      <div className="container header__container">
         <div className="header__left">
           <div className="logo-wrap">
             <div className="logo-wrap__image">
@@ -22,6 +29,54 @@ function Header() {
                   {/* //TODO: Change "a" to <Link> react-router componnent */}
                   <a className="logo-wrap__city-link">м. Львів</a>
                 </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="header-info">
+            <div className="header-info__image">
+              <img src={deliverySvg} alt="" className="header-info__svg" />
+            </div>
+            <div className="header-info__content">
+              <div className="header-info__line">
+                Доставка по місту —{" "}
+                <span className="header-info__line_bolder">40 ₴</span>
+              </div>
+              <div className="header-info__line">
+                За межі міста —{" "}
+                <span className="header-info__line_bolder">+10 ₴/км</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="header__right">
+          <div className="header-info">
+            <div className="header-info__image">
+              <img src={clockSvg} alt="" className="header-info__svg" />
+            </div>
+            <div className="header-info__content">
+              <div className="header-info__line">
+                <span className="header-info__line_bolder">Пн-Пт:</span>{" "}
+                09:00—22:00
+              </div>
+              <div className="header-info__line">
+                <span className="header-info__line_bolder">Сб-Нд:</span>{" "}
+                10:00—23:00
+              </div>
+            </div>
+          </div>
+
+          <div className="header-info">
+            <div className="header-info__image">
+              <img src={callSvg} alt="" className="header-info__svg" />
+            </div>
+            <div className="header-info__content">
+              <div className="header-info__line">
+                <a className="header-info__link">(032) 662 58 47</a>
+              </div>
+              <div className="header-info__line">
+                <a className="header-info__link">(050) 420 52 10</a>
               </div>
             </div>
           </div>
